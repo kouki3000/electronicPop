ebcd52547275750ce45116de5bad434fa0a9d892
kouki3000 <kouki1105@gmail.com>
2019-08-09 11:57:07 +0900

    dynamoDBにつなぐとちゅう

--- app/src/main/java/com/example/electronicpop/MainActivity.java
+++ app/src/main/java/com/example/electronicpop/MainActivity.java
@@ -1,14 +1,102 @@
 package com.example.electronicpop;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.icu.text.DateFormat;
+import android.icu.text.SimpleDateFormat;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBMapper;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBScanExpression;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ComparisonOperator;
+import com.amazonaws.services.dynamodbv2.model.Condition;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    private pop_db setData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Amazon Cognito 認証情報プロバイダーを初期化します
+        CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
+                getApplicationContext(),
+                "ap-northeast-1:bacfdb0c-ccc4-454b-9949-10bf45527c7d", // ID プールの ID
+                Regions.AP_NORTHEAST_1 // リージョン
+        );
+        AmazonDynamoDBClient ddbClient = new AmazonDynamoDBClient(credentialsProvider);
+
+        Region apNortheast1 = Region.getRegion(Regions.AP_NORTHEAST_1);
+        ddbClient.setRegion(apNortheast1);
+
+        final DynamoDBMapper mapper = new DynamoDBMapper(ddbClient);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                //final DateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+                //final Date date = new Date(System.currentTimeMillis());
+
+
+                pop_db setData = new pop_db();
+                setData.setPop_id("iphone_xs_001");
+                setData.setProduct_name("iphone_xs");
+                setData.setPrice(20000);
+                setData.setDown_payment(100);
+                setData.setAdvertisement("今ならお得！！！！");
+                setData.setUser_id("user0001");
+                setData.setCreated_at("time");
+                setData.setUpdate_at("time2");
+                setData.setTemplate_type(0);
+                setData.setProduct_info_url("http://product/info/");
+                System.out.println(setData);
+                mapper.save(setData);
+                /*
+                // 全レコード取得方法
+                List<pop_db> newsItem = mapper.scan(pop_db.class, new DynamoDBScanExpression());
+
+                // Item read
+                Log.d("News Item:", newsItem.toString());
+                for (pop_db item : newsItem) {
+                    System.out.format("ID=%s, Price=%s %n",
+                            item.getPop_id(), item.getPrice());
+                }
+                pop_db item = mapper.load(pop_db.class, 11);
+                System.out.println("---------");
+                System.out.println(item);
+                //item.setPrice(1413240);
+                //mapper.save(item);
+
+                // Priceを指定してscanする方法
+                Map<String, Condition> conditions = new HashMap<String, Condition>();
+                Condition priceCondition = new Condition()
+                        .withComparisonOperator(ComparisonOperator.EQ.toString())
+                        .withAttributeValueList(new AttributeValue().withN(Integer.toString(100)));
+                conditions.put("Price", priceCondition);
+                DynamoDBScanExpression scanExpression = new DynamoDBScanExpression();
+                scanExpression.setScanFilter(conditions);
+                List<pop_db> newItems = mapper.scan(pop_db.class, scanExpression);
+                Log.d("News Item:", newsItem.toString());
+                for (pop_db item2 : newItems) {
+                    System.out.format("ID=%s, Price=%s %n",
+                            item2.getPop_id(), item2.getPrice());
+                }
+                */
+            }
+
+        }).start();
     }
 }
---
+++ app/src/main/java/com/example/electronicpop/pop_db.java
@@ -0,0 +1,62 @@
+package com.example.electronicpop;
+import android.os.StatFs;
+
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.*;
+
+@DynamoDBTable(tableName = "pop_test_nakamura")
+public class pop_db {
+
+    private String pop_id;
+    private String product_name;
+    private int price;
+    private int down_payment;
+    private String advertisement; //　後でlistにする
+    private String user_id;
+    private String created_at;
+    private String update_at;
+    private int template_type;
+    private String product_info_url;
+
+    /* パーティションキーで指定した属性名 */
+    @DynamoDBHashKey(attributeName = "pop_id")
+    public String getPop_id() {return pop_id; }
+    public void setPop_id(String pop_id) { this.pop_id = pop_id; }
+
+    /* 任意の属性名 AWSコンソールで事前定義不要*/
+    @DynamoDBAttribute(attributeName = "product_name")
+    public String getProduct_name() {return product_name; }
+    public void setProduct_name(String product_name) { this.product_name = product_name; }
+
+    @DynamoDBAttribute(attributeName = "price")
+    public int getPrice() {return price; }
+    public void setPrice(int price) { this.price = price; }
+
+    @DynamoDBAttribute(attributeName = "down_payment")
+    public int getDown_payment() {return down_payment; }
+    public void setDown_payment(int down_payment) { this.down_payment = down_payment; }
+
+    @DynamoDBAttribute(attributeName = "advertisement")
+    public String getAdvertisement() {return advertisement; }
+    public void setAdvertisement(String advertisement) { this.advertisement = advertisement; }
+
+    @DynamoDBAttribute(attributeName = "user_id")
+    public String getUser_id() {return user_id; }
+    public void setUser_id(String user_id) { this.user_id = user_id; }
+
+    @DynamoDBAttribute(attributeName = "created_at")
+    public String getCreated_at() {return created_at; }
+    public void setCreated_at(String created_at) { this.created_at = created_at; }
+
+    @DynamoDBAttribute(attributeName = "update_at")
+    public String getUpdate_at() {return update_at; }
+    public void setUpdate_at(String update_at) { this.update_at = update_at; }
+
+    @DynamoDBAttribute(attributeName = "template_type")
+    public int getTemplate_type() {return template_type; }
+    public void setTemplate_type(int template_type) { this.template_type = template_type; }
+
+    @DynamoDBAttribute(attributeName = "product_info_url")
+    public String getProduct_info_url() {return product_info_url; }
+    public void setProduct_info_url(String product_info_url) { this.product_info_url = product_info_url; }
+
+}
